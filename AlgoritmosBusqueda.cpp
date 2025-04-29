a71eec73180232e37509f2412693d941e68cc504
javiiariass <javierariasfuentes@gmail.com>
2025-04-29 18:08:38 +0200

    intento de salvar la práctica xd

--- AlgoritmosBusqueda.cpp
+++ AlgoritmosBusqueda.cpp
@@ -3,36 +3,41 @@
 int AlgoritmosBusqueda::busquedaBinaria1(int A[], int izq, int der, int x)
 {
 
-    if (izq > der) {
+    if (izq > der) {  // 1 OE comparación
         return -1;
     } else {
-        int medio = (izq + der) / 2;
-        if (A[medio] == x) {
+        int medio = (izq + der) / 2; // 3 OE -> Asignación y 2 operaciones aritméticas
+        if (A[medio] == x) { // 2 OE -> acceso al vector y comparación
             return medio;
-        } else if (A[medio] > x) {
-            return busquedaBinaria1(A, izq, medio - 1, x);
+        } else if (A[medio] > x) { // 2 OE -> acceso al vector y comparación
+            return busquedaBinaria1(A, izq, medio - 1, x); // 2 OE -> llamada a función y operación aritmética + Recursividad
         } else {
-            return busquedaBinaria1(A, medio + 1, der, x);
+            return busquedaBinaria1(A, medio + 1, der, x); // 2 OE -> llamada a función y operación aritmética + Recursividad
         }
     }
+
+    // 1 OE de cualquier return en cualquier caso. Arriba no las tuve en cuenta para hacerlo aquí
     
 }
 
 int AlgoritmosBusqueda::busquedaBinaria2(int A[], int izq, int der, int x)
 {
 
-    if (izq > der) {
+    if (izq > der) { // 1 OE comparación
         return -1;
     } else {
-        int medio = (izq + der) / 2;
-        if (A[medio] == x && (medio == izq || A[medio - 1] != x)) { // AND (medio = izq OR A[medio-1] ≠ x)
+        int medio = (izq + der) / 2; // 3 OE -> Asignación y 2 operaciones aritméticas
+        if (A[medio] == x && (medio == izq || A[medio - 1] != x)) {  
+            // 2 OE -> 2 accesos al vector, 3 comparaciones, 2 op lógicas, 1 op aritmética
             return medio;
-        } else if (A[medio] >= x) {
-            return busquedaBinaria2(A, izq, medio - 1, x);
+        } else if (A[medio] >= x) { // 2 OE -> acceso al vector y comparación
+            return busquedaBinaria2(A, izq, medio - 1, x); // 2 OE -> llamada a función y operación aritmética + Recursividad
         } else {
-            return busquedaBinaria2(A, medio + 1, der, x);
+            return busquedaBinaria2(A, medio + 1, der, x); // 2 OE -> llamada a función y operación aritmética + Recursividad
         }
+    
     }
+    // 1 OE de cualquier return en cualquier caso. Arriba no las tuve en cuenta para hacerlo aquí
     
 }
 
